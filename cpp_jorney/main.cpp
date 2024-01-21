57655129f63b69e2c7aa0b51952a0f3a54c5b65c
Novosad Ivan <novosad-94@list.ru>
2024-01-21 11:12:35 +0300

    leetcode dailyTask on c++ & python

--- cpp_jorney/main.cpp
+++ cpp_jorney/main.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
-
+// check if number or string
+bool IsDigit(string c)
+{
+    if (isdigit(c[0]) == false)
+        return false;
+    return true;
+}
 int main()
 {
-    for (size_t i = 0; i <= 15; i++)
-    {
-        cout << "\t" << hex << uppercase << i;
-    }
-    cout << endl;
-
-    for (short n = 2; n < 8; n++)
-    {
-        cout << n;
-
-        for (short i = 0; i < 16; i++)
-        {
-            cout << "\t" << char(n*16 + i);
-        }
-        cout << endl;
-        
-    }
+    string line;
+    getline(cin, line);
+    cout << IsDigit(line) << endl;
     
-
     return 0;
 }
+
