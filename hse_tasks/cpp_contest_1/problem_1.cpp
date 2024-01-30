a8b66dbf00f253b3c1cd960a7e3c7bb771e6af8a
Novosad Ivan <novosad-94@list.ru>
2024-01-30 14:29:50 +0300

    cpp seminar HSE

--- hse_tasks/cpp_contest_1/problem_1.cpp
+++ hse_tasks/cpp_contest_1/problem_1.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
+
 using namespace std;
 
-int check(int x1, int x2, int y1, int y2)
+bool canQueenMoveToCell(int x1, int x2, int y1, int y2)
 {
     if (y1 == x1 || y2 == x2 || abs(x1 - y1) == abs(x2 - y2))
     {
-        cout << "YES" << endl;
+        return true;
     }
-    else
-    {
-        cout << "NO" << endl;
-    }
-    return 0;
+
+    return false;
 }
 
 int main()
 {
-    int x1, x2, y1, y2;
-    cin >> x1;
-    cin >> x2;
-    cin >> y1;
-    cin >> y2;
+    size_t x1, x2, y1, y2;
+    cin >> x1 >> x2 >> y1 >> y2;
 
-    check(x1, x2, y1, y2);
+    cout << (canQueenMoveToCell(x1, x2, y1, y2) ? "YES" : "NO") << '\n';
+
     return 0;
 }
