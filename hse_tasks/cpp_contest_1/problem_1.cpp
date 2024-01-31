df7282307f090c5aecbfc523ca020bbdec787851
Novosad Ivan <novosad-94@list.ru>
2024-01-31 10:08:53 +0300

    leetcode daily on python & cpp

--- hse_tasks/cpp_contest_1/problem_1.cpp
+++ hse_tasks/cpp_contest_1/problem_1.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
 bool canQueenMoveToCell(int x1, int x2, int y1, int y2)
 {
     if (y1 == x1 || y2 == x2 || abs(x1 - y1) == abs(x2 - y2))
@@ -14,6 +12,7 @@ bool canQueenMoveToCell(int x1, int x2, int y1, int y2)
 
 int main()
 {
+    using namespace std;
     size_t x1, x2, y1, y2;
     cin >> x1 >> x2 >> y1 >> y2;
 
