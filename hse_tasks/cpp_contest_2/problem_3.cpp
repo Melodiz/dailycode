7a5b2d4aab2fcf7303ffa1f561e0314e5223fb26
Novosad Ivan <novosad-94@list.ru>
2024-02-06 23:19:11 +0300

    hse cpp contest: few problems

--- hse_tasks/cpp_contest_2/problem_3.cpp
+++ hse_tasks/cpp_contest_2/problem_3.cpp
@@ -1,6 +1,50 @@
 #include "iostream"
 #include <string>
 #include <algorithm>
+#include <set>
 using namespace std;
 
+bool check(string data);
+string removeDumnLetters(string data);
 
+string removeDumnLetters(string data)
+{
+    string res = "";
+    set<char> letters;
+    letters.insert('a');
+    letters.insert('y');
+    letters.insert('e');
+    letters.insert('h');
+    letters.insert('i');
+    letters.insert('o');
+    letters.insert('u');
+    letters.insert('w');
+
+    // hate it, cau'se i can not make a set with values
+    // like that: set<char> letters {'a', 'e', 'h', 'i', 'o', 'u', 'w', 'y'};
+
+    for (size_t i = 0; i < data.length(); i++)
+    {
+        // bool found = (find(letters.begin(), letters.end(), data[i]) != letters.end());
+        if(!(letters.find(data[i]) != letters.end()))
+        {
+            res += data[i];
+        }
+    }
+    return res;
+}
+
+bool check(string data)
+{
+    char firstLetter = data[0];
+    return false;
+}
+
+int main()
+{
+    string word;
+    cin >> word;
+    cout << removeDumnLetters(word) << endl;
+
+    return 0;
+}
