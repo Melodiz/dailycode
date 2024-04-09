53572487cd481683174656770296234ae3806d23
Novosad Ivan <novosad-94@list.ru>
2024-04-09 14:03:35 +0300

    leetcode daily task on cpp

---
+++ leetcode/Time_Needed_to_Buy_Tickets.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k)
+    {
+        int i = 0;
+        int ans = 0;
+        int n = tickets.size()
+        while (tickets[k])
+        {
+            if (tickets[i%n])
+            {
+               tickets[i%n]--; 
+               ans++;
+            }
+            i++;
+        }
+        return i;
+    }
+};
