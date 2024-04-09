4bc14c99cdcaa03ee267534c4d9d58742bce43da
Novosad Ivan <novosad-94@list.ru>
2024-04-09 14:38:31 +0300

    solve several leetcode task

---
+++ leetcode/Majority_Element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0;
+        int ans = 0;
+        
+        for (size_t num : nums) {
+            if (freq == 0) {
+                ans = num;
+            }
+            
+            if (num == ans) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        
+        return ans;
+    }
+};
