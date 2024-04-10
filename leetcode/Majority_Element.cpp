ad3f4caad801b2ed05c25d9819111eb2ea0446d7
Novosad Ivan <novosad-94@list.ru>
2024-04-10 13:32:25 +0300

    leetcode daily on cpp

--- leetcode/Majority_Element.cpp
+++ leetcode/Majority_Element.cpp
@@ -1,21 +1,28 @@
-class Solution {
+class Solution
+{
 public:
-    int majorityElement(vector<int>& nums) {
+    int majorityElement(vector<int>& nums)
+    {
         int freq = 0;
         int ans = 0;
-        
-        for (size_t num : nums) {
-            if (freq == 0) {
+
+        for (size_t num: nums)
+        {
+            if (freq == 0)
+            {
                 ans = num;
             }
-            
-            if (num == ans) {
+
+            if (num == ans)
+            {
                 freq++;
-            } else {
+            }
+            else
+            {
                 freq--;
             }
         }
-        
+
         return ans;
     }
 };
--- python_quick/main.py
+++ python_quick/main.py
@@ -35,7 +35,7 @@ def col_change(data, n1, n2):
 def change(data, n1, n2):
     n1 -= 1
     n2 -= 1
-    data = row_change(data, n1, n2)
+    # data = row_change(data, n1, n2)
     data = col_change(data, n1, n2)
     print(f'change {n1+1} with {n2+1}')
     for j in range(3):
@@ -62,7 +62,7 @@ def col_sub(data, n1, n2, coef):
 def sub(data, n1, n2, coef):
     n1 -= 1
     n2 -= 1
-    data = row_sub(data, n1, n2, coef)
+    # data = row_sub(data, n1, n2, coef)
     data = col_sub(data, n1, n2, coef)
     print(f'sub {coef}*{n1+1} from {n2+1}')
     for j in range(3):
@@ -75,8 +75,8 @@ def sub(data, n1, n2, coef):
 
 def mult(data, n1, coef):
     n1 -= 1
-    for i in range(3):
-        data[n1][i] *= coef
+    # for i in range(3):
+    #     data[n1][i] *= coef
     for i in range(3):
         data[i][n1] *= coef
     print(f'Mult {n1+1} on {coef}')
@@ -97,10 +97,26 @@ def to_int(data):
     return data
 
 
+# data = [
+#     [1, 1, 1],
+#     [1, 1, -1],
+#     [1, -1, 1],
+# ]
+# data = change(data, 2, 3)
+# data = sub(data, 2, 3, 1)
+# data = sub(data, 1, 3, -1)
+# data = sub(data, 1, 2, -1)
+# data = sub(data, 1, 3, -1)
+# data = mult(data, 3, -1/2)
+# data = to_int(data)
+# data = change(data, 2, 3)
+# data = sub(data, 2, 3, 1)
+
+print('______________')
 data = [
-    [1, 1, 1],
-    [1, 1, -1],
-    [1, -1, 1],
+    [1, 0, 0], 
+    [0, 1, 0],
+    [0, 0, 1]
 ]
 data = change(data, 2, 3)
 data = sub(data, 2, 3, 1)
@@ -122,13 +138,3 @@ data = sub(data, 2, 3, 1)
 #     n2 = random.choice([1, 2, 3])
 
 #     data = change(data, n1, n2)
-
-# g(a + g - k) + k(a - g + k) + a(a + g + k) = 1,
-# h(a + g - k) + b(a + g + k) + (a - g + k)p = 0,
-# f(a - g + k) + c(a + g + k) +(a + g - k)m = 0,
-# g(b + h - p) + k(b - h + p) + a(b + h + p) = 0,
-# h(b + h - p) + p(b - h + p) + b(b + h + p) = -1,
-# m(b + h - p) + f(b - h + p) + c(b + h + p) = 0,
-# k(c + f - m) + g(c - f + m) + a(c + f + m) = 0,
-# h(c - f + m) + b(c + f + m) + (c + f - m)p = 0,
-# f(c + f - m) + m(c - f + m) + c(c + f + m) = 1
