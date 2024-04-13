9e5d13e09e1883970fd08d63823a70971fdee558
Novosad Ivan <novosad-94@list.ru>
2024-04-13 14:28:10 +0300

    leetcode daily on python

---
+++ leetcode/Valid_Perfect_Square.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2)
+    {
+        sort(nums1.begin(), nums1.end());
+        nums1.erase(unique(nums1.begin(), nums1.end()), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        nums2.erase(unique(nums2.begin(), nums2.end()), nums2.end());
+        vector<int> v_intersection;
+        set_intersection(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(),
+                         back_inserter(v_intersection));
+        return v_intersection;
+    }
+};
