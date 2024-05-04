176e1270921b0dccbe77f364f1eb057c6728ab2f
Novosad Ivan <novosad-94@list.ru>
2024-05-04 12:10:27 +0300

    leetcode daily

--- leetcode/May 2024/Largest_Positive_Integer_That_Exists_With_Its_Negative.cpp
+++ leetcode/May 2024/Largest_Positive_Integer_That_Exists_With_Its_Negative.cpp
@@ -1,15 +1,21 @@
-class Solution:
-    def findMaxK(self, nums: List[int]) -> int:
-        nums.sort()
-        left, right = 0, len(nums) - 1
-        max_k = float('-inf')
-        while left < right:
-            if nums[left] + nums[right] == 0:
-                max_k = max(max_k, nums[right])
-                left += 1
-                right -= 1
-            elif nums[left] + nums[right] < 0:
-                left += 1
-            else:
-                right -= 1
-        return max_k if max_k != float('-inf') else -1  # If no such pair found
+class Solution {
+public:
+    int findMaxK(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int left = 0, right = nums.size() - 1;
+        int maxK = std::numeric_limits<int>::min();
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == 0) {
+                maxK = std::max(maxK, nums[right]);
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxK != std::numeric_limits<int>::min() ? maxK : -1;  // If no such pair found
+    }
+};
