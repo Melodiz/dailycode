58a32a48a1aad147a00af01bebefeac0adf1dc69
Novosad Ivan <novosad-94@list.ru>
2024-05-13 08:10:09 +0300

    leetcode daily

--- leetcode/May 2024/Score After Flipping Matrix.cpp
+++ leetcode/May 2024/Score After Flipping Matrix.cpp
@@ -8,7 +8,6 @@ public:
         for(int j = 1; j < m; ++j) {
             int val = 1 << (m - 1 - j);
             int set = 0;
-
             for(int i = 0; i < n; ++i) {
                 if(grid[i][j] == grid[i][0]) {
                     set++;
