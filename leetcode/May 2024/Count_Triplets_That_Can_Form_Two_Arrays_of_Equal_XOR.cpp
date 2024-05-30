d01e96ef2c0bda1367f0c64baef4054464a5ecae
Novosad Ivan <novosad-94@list.ru>
2024-05-30 13:18:31 +0300

    leetcode daily

---
+++ leetcode/May 2024/Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int countTriplets(vector<int>& arr)
+    {
+        int count = 0;
+        int n = arr.size();
+        vector<int> prefixXor(n + 1, 0);
+        
+        for (int i = 0; i < n; ++i)
+        {
+            prefixXor[i + 1] = prefixXor[i] ^ arr[i];
+        }
+        
+        for (int i = 0; i < n; ++i)
+        {
+            for (int k = i + 1; k < n; ++k)
+            {
+                if (prefixXor[i] == prefixXor[k + 1])
+                {
+                    count += (k - i);
+                }
+            }
+        }
+        
+        return count;
+    }
+};
