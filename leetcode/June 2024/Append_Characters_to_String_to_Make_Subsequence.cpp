29c833f1d675394b5de1340c6d3b6ec3ca39ec39
Novosad Ivan <novosad-94@list.ru>
2024-06-03 11:02:41 +0300

    leetcode daily

---
+++ leetcode/June 2024/Append_Characters_to_String_to_Make_Subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int appendCharacters(string s, string t)
+    {
+        int index = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == t[index]) { index++; }
+        }
+        return t.size() - index;
+    }
+};
