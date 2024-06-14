883ba26c5eda495c4cddfe76c604348be91483a5
Novosad Ivan <novosad-94@list.ru>
2024-06-14 13:04:10 +0300

    leetcode daily

---
+++ leetcode/June 2024/Minimum_Increment_to_Make_Array_Unique.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int minIncrementForUnique(vector<int>& nums)
+    {
+        int n = nums.size();
+        if (n == 0)
+            return 0;
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i] <= nums[i - 1])
+            {
+                ans += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return ans;
+    }
+};
