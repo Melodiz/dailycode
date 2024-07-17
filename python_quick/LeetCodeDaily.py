0ab39898dfaf2478b5e6a7a5efa83739029917e1
Novosad Ivan <novosad-94@list.ru>
2024-07-17 18:33:16 +0300

    cleanUp

--- python_quick/LeetCodeDaily.py
+++
@@ -1,8 +0,0 @@
-for x in range(-150, 150):
-    for y in range(-150, 150):
-        x = x/100
-        y = y/100
-        if (x**3 + y**3)**1/3 > x**2 + y**2:
-            print(x,y)
-
-print('end')
--- python_quick/yandexEntry/task5.py
+++
@@ -1,4 +0,0 @@
-import math
-a = float(input())
-b = float(input())
-print(a**2/b**2/(math.atan(1)*4))
