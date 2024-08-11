a94098c904a5db14c30822396b98b2ad094ecd98
Novosad Ivan <novosad-94@list.ru>
2024-08-11 11:02:24 +0300

    leetcode daily redeem

--- leetcode/August_2024/Minimum_Number_of_Days_to_Disconnect_Island.cpp
+++ leetcode/August_2024/Minimum_Number_of_Days_to_Disconnect_Island.cpp
@@ -1,53 +1,63 @@
+#include <vector>
+#include <functional>
+#include <utility>
+
 class Solution
 {
 public:
-    int minDays(vector<vector<int>>& grid)
+    int minDays(std::vector<std::vector<int>>& grid)
     {
-        auto count_islands = [&]() -> int {
-            vector<vector<int>> seen(grid.size(), vector<int>(grid[0].size(), 0));
-            int islands = 0;
+        // Lambda function to count the number of islands in the grid
+        auto countIslands = [&]() -> int {
+            std::vector<std::vector<int>> visited(grid.size(), std::vector<int>(grid[0].size(), 0));
+            int islandCount = 0;
 
-            function<void(int, int)> dfs = [&](int r, int c) {
-                seen[r][c] = 1;
-                for (auto [dr, dc]: {pair{-1, 0}, {1, 0}, {0, -1}, {0, 1}})
+            // Depth-First Search (DFS) to mark all cells in the current island
+            std::function<void(int, int)> dfs = [&](int row, int col) {
+                visited[row][col] = 1;
+                for (auto [dRow, dCol] : {std::pair{-1, 0}, {1, 0}, {0, -1}, {0, 1}})
                 {
-                    int nr = r + dr, nc = c + dc;
-                    if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() && grid[nr][nc] == 1 && !seen[nr][nc])
+                    int newRow = row + dRow, newCol = col + dCol;
+                    if (newRow >= 0 && newRow < grid.size() && newCol >= 0 && newCol < grid[0].size() && grid[newRow][newCol] == 1 && !visited[newRow][newCol])
                     {
-                        dfs(nr, nc);
+                        dfs(newRow, newCol);
                     }
                 }
             };
 
-            for (int i = 0; i < grid.size(); i++)
+            // Iterate through the grid to find and count all islands
+            for (int r = 0; r < grid.size(); r++)
             {
-                for (int j = 0; j < grid[0].size(); j++)
+                for (int c = 0; c < grid[0].size(); c++)
                 {
-                    if (grid[i][j] == 1 && !seen[i][j])
+                    if (grid[r][c] == 1 && !visited[r][c])
                     {
-                        islands++;
-                        dfs(i, j);
+                        islandCount++;
+                        dfs(r, c);
                     }
                 }
             }
-            return islands;
+            return islandCount;
         };
 
-        if (count_islands() != 1) return 0;
+        // If the initial grid does not have exactly one island, return 0
+        if (countIslands() != 1) return 0;
 
-        for (int i = 0; i < grid.size(); i++)
+        // Try removing each land cell to see if it disconnects the island
+        for (int r = 0; r < grid.size(); r++)
         {
-            for (int j = 0; j < grid[0].size(); j++)
+            for (int c = 0; c < grid[0].size(); c++)
             {
-                if (grid[i][j] == 1)
+                if (grid[r][c] == 1)
                 {
-                    grid[i][j] = 0;
-                    if (count_islands() != 1) return 1;
-                    grid[i][j] = 1;
+                    grid[r][c] = 0; // Temporarily remove the land cell
+                    if (countIslands() != 1) return 1; // If the island count changes, return 1
+                    grid[r][c] = 1; // Restore the land cell
                 }
             }
         }
 
+        // If no single cell removal disconnects the island, return 2
         return 2;
     }
 };
