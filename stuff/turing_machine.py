2d1d83638c701a27b5654a1f822dbff432f8fd0d
Novosad Ivan <novosad-94@list.ru>
2024-12-05 23:15:57 +0300

    update distribution-plot module

--- stuff/turing_machine.py
+++ stuff/turing_machine.py
@@ -1,63 +1,38 @@
 class TuringMachine:
-    def __init__(self, tape, initial_state, bls='0'): # bls = blank symbol
+    def __init__(self, tape):
         self.tape = list(tape)
-        self.head_position = 0
-        self.current_state = initial_state
-        self.blank_symbol = bls
-        self.rules = {
-            ('q0', '0'): ('0', 'R', 'q0'),  
-            ('q0', '1'): ('1', 'R', 'q0'), 
-            ('q0', bls): (bls, 'L', 'q1'),
-            ('q1', '0'): ('1', 'L', 'q3'),
-            ('q1', 'q'): ('0', 'L', 'HALT'),
-        }
+        self.head = 0
+        self.state = 'start'
+        self.period_length = 1
 
     def step(self):
-        if self.current_state == 'HALT':
-            return False
+        if self.state == 'start':
+            if self.period_length >= len(self.tape):
+                self.state = 'reject'
+            else:
+                self.state = 'check_period'
+                self.head = 0
 
-        current_symbol = self.tape[self.head_position] if self.head_position < len(self.tape) else self.blank_symbol
-        action = self.rules.get((self.current_state, current_symbol))
+        elif self.state == 'check_period':
+            if self.head + self.period_length < len(self.tape):
+                if self.tape[self.head] == self.tape[self.head + self.period_length]:
+                    self.head += 1
+                else:
+                    self.state = 'next_period'
+            else:
+                self.state = 'accept'
 
-        if action:
-            write_symbol, move_direction, next_state = action
-            if self.head_position < len(self.tape):
-                self.tape[self.head_position] = write_symbol
-            elif write_symbol != self.blank_symbol:
-                self.tape.append(write_symbol)
-
-            self.current_state = next_state
-
-            if move_direction == 'R':
-                self.head_position += 1
-            elif move_direction == 'L':
-                self.head_position -= 1
-                if self.head_position < 0:
-                    self.tape.insert(0, self.blank_symbol)
-                    self.head_position = 0
-
-            # Print the current state of the Turing machine
-            print(f"State: {self.current_state}, Head Position: {self.head_position}, Tape: {''.join(self.tape)}")
-
-            return True
-        else:
-            return False
+        elif self.state == 'next_period':
+            self.period_length += 1
+            self.state = 'start'
 
     def run(self):
-        counter = 0
-        while self.step() and counter < 100:  # Maximum number of steps
-            counter += 1
-            pass
-
-    def get_tape(self):
-        # Remove leading and trailing blank symbols
-        return ''.join(self.tape).strip(self.blank_symbol)
-
+        while self.state not in ['accept', 'reject']:
+            self.step()
+        return self.state
 
 # Example usage
-tape = "10111"
-tm = TuringMachine(tape, 'q0', bls='B')  # Using 'B' as the blank symbol
-print("Initial tape:", tape)
-tm.run()
-print("Final Tape:", tm.get_tape())
-print("Final State:", tm.current_state)
+word = "bbab"
+tm = TuringMachine(word)
+result = tm.run()
+print(f"The word '{word}' is {'periodic' if result == 'accept' else 'not periodic'}.")
